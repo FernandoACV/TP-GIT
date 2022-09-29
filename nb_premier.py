e5f9079fa84a5f3a7f3d8fc73fd0bc04fb32b6c8
Yanis <yanisbo213@gmail.com>
2022-09-29 12:37:42 +0200

    bonus script nombres premiers

--- nb_premier.py
+++ nb_premier.py
@@ -14,8 +14,21 @@ def nb_premier(x, y):
     return prime_list
  
 # main
-x = 1
-y = 1000000
+bx=False
+by=False
+while(bx==False):
+    x = int (input("entrez un nombre entier positif supérieur à 0 : "))
+    if (x>0):
+        bx=True 
+    else:
+        print("veuillez entrer un nombre valide")
+while(by==False):
+    y = int (input ("entrez un nombre entier positif supérieur au précédent : "))
+    if (y>x):
+         by=True
+    else:
+        print("veuillez entrer un nombre valide")
+
 lst = nb_premier(x, y)
 if len(lst) == 0:
     print("Il n'y a pas de nombres premier dans cet intervalle")
