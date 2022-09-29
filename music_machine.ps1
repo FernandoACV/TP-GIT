5c521e288aafc9904315d56cbc48d59b18201009
Fernando Vieira <fernando.cerqueiravieira@gmail.com>
2022-09-29 12:38:23 +0200

    bonus: add music_machine

---
+++ music_machine.ps1
@@ -0,0 +1,386 @@
+﻿$starwars = "   ______________    ____     _       _____    ____  _____    ________  __________  _________
+  / ___/_  __/   |  / __ \   | |     / /   |  / __ \/ ___/   /_  __/ / / / ____/  |/  / ____/
+  \__ \ / / / /| | / /_/ /   | | /| / / /| | / /_/ /\__ \     / / / /_/ / __/ / /|_/ / __/   
+ ___/ // / / ___ |/ _, _/    | |/ |/ / ___ |/ _, _/___/ /    / / / __  / /___/ /  / / /___   
+/____//_/ /_/  |_/_/ |_|     |__/|__/_/  |_/_/ |_|/____/    /_/ /_/ /_/_____/_/  /_/_____/   
+                                                                                           "
+$anniversaire ="
+       _                                                  _                            _           __
+      (_)___  __  _____  __  ___  __   ____ _____  ____  (_)   _____  ______________ _(_)_______  / /
+     / / __ \/ / / / _ \/ / / / |/_/  / __ `/ __ \/ __ \/ / | / / _ \/ ___/ ___/ __ `/ / ___/ _ \/ / 
+    / / /_/ / /_/ /  __/ /_/ />  <   / /_/ / / / / / / / /| |/ /  __/ /  (__  ) /_/ / / /  /  __/_/  
+ __/ /\____/\__, /\___/\__,_/_/|_|   \__,_/_/ /_/_/ /_/_/ |___/\___/_/  /____/\__,_/_/_/   \___(_)   
+/___/      /____/                                                                                    
+"
+
+$pacman ="
+
+                                          
+    ____  ____ __________ ___  ____ _____ 
+   / __ \/ __ `/ ___/ __ `__ \/ __ `/ __ \
+  / /_/ / /_/ / /__/ / / / / / /_/ / / / /
+ / .___/\__,_/\___/_/ /_/ /_/\__,_/_/ /_/ 
+/_/                                       
+
+
+================================================.
+     .-.   .-.     .--.                         |
+    | OO| | OO|   / _.-' .-.   .-.  .-.   .''.  |
+    |   | |   |   \  '-. '-'   '-'  '-'   '..'  |
+    '^^^' '^^^'    '--'                         |
+===============.  .-.  .================.  .-.  |
+               | |   | |                |  '-'  |
+               | |   | |                |       |
+               | ':-:' |                |  .-.  |
+               |  '-'  |                |  '-'  |
+==============='       '================'       |
+"
+
+$halloween ="
+    __  __      ____                              __
+   / / / /___ _/ / /___ _      _____  ___  ____  / /
+  / /_/ / __ `/ / / __ \ | /| / / _ \/ _ \/ __ \/ / 
+ / __  / /_/ / / / /_/ / |/ |/ /  __/  __/ / / /_/  
+/_/ /_/\__,_/_/_/\____/|__/|__/\___/\___/_/ /_(_)   
+
+⠀⠀⠀⢀⣠⣤⣶⣷⣿⣾⣦⣤⣤⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⢴⣾⣿⣿⠟⠛⠛⠻⠿⣿⣿⣿⣿⣶⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣴⣾⣿⣿⣿⣶⣶⣶⣶⣦⡤⠀⠀⠀
+⠀⣴⣿⣿⡟⣡⣴⣶⣶⣶⣤⣄⠉⢿⣿⣿⣧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣾⣿⣿⣿⣿⠿⠿⣿⣿⣿⣿⣿⣿⣷⡀⠀
+⣼⣿⣿⡟⣰⣿⣿⣿⣿⣿⣿⣿⣷⡀⠙⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⣸⣿⣿⣿⠟⠉⠀⠀⠀⠀⠀⠈⠙⠻⣿⣿⣷⡄
+⣻⣿⣿⡇⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠸⣿⡇⠀⠀⠀⠀⣠⠀⠀⠀⣿⣿⣿⠃⠀⠀⣠⣾⣿⣿⣿⣶⣤⡀⣿⣿⣿⡄
+⠿⣿⣿⣿⣌⠻⣿⣿⣿⣿⣿⣿⣿⠟⠀⢠⣿⠇⠀⠀⠀⣤⣯⠀⠀⠀⣿⣿⡇⠀⠀⣼⣿⣿⣿⣿⣿⣿⣿⡇⣿⣿⣿⡇
+⠐⢿⣿⣿⣿⣷⣌⡙⠛⠻⠛⠋⢁⣠⣴⣿⠏⠀⢀⣴⣿⣿⣿⣷⡀⠀⢹⣿⣷⠀⠀⢿⣿⣿⣿⣿⣿⣿⣿⢡⣿⣿⣿⠀
+⠀⠝⢻⣿⣿⣿⣿⣿⣿⣶⣶⣾⣿⣿⣿⠋⢀⣴⣿⣿⣿⣿⣿⣿⣷⠀⠙⣿⣿⣷⣄⡈⠛⠛⠛⠛⢛⣫⣵⣿⣿⣿⡇⠀
+⠀⠄⠀⠛⣿⢿⣿⣿⣿⣿⣿⠟⠉⠀⠁⠠⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠐⠿⢿⣿⣿⣿⣷⣶⣶⣿⣿⣿⣿⣿⣿⣿⠃⠀
+⠀⢠⣤⣄⣛⣹⣿⣇⣙⡏⠁⠀⠀⠀⠀⠀⠻⢿⠛⠻⢿⢿⣿⣿⣿⡇⠀⠀⠀⠙⠿⣿⡟⠭⣿⣿⣿⣿⣿⣿⡿⠃⠀⠀
+⠀⠀⠻⣿⣿⣿⣿⣿⣿⣿⣶⣶⣤⣤⣀⣀⣀⡘⠀⠀⠀⠀⠐⢿⡿⠁⠀⠀⠀⠀⠀⣸⠃⠀⣨⣴⣿⣿⣿⣿⠇⠀⠀⠀
+⠀⠀⠀⠈⢿⣿⣿⣿⣿⣿⣿⣧⡟⠹⣉⡿⠋⠻⡿⠻⢷⡶⠦⣾⣇⣀⣀⣀⣴⣶⣶⣿⣷⣾⣿⣿⡿⢿⣿⠏⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠸⣿⣿⣿⣾⣿⣩⣷⣤⣨⣧⡀⢀⠇⠀⠀⡇⠀⠀⢿⠀⢿⣭⣿⣿⣼⣿⣿⣿⡿⠋⠀⢸⡟⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣶⣾⣿⣶⣶⣧⣤⣼⣷⣿⣿⣿⣿⣿⠏⠀⠀⠀⠈⠇⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⣿⡈⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠈⠣⠀⠘⠻⣿⣿⣿⢿⣿⣿⣿⣿⡿⢿⣿⢿⣿⣿⣿⣿⣿⣿⠿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⢿⣷⣤⣧⣀⠀⡇⠀⠀⣧⠀⠸⠀⠘⢿⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠹⣿⣿⣿⣿⣿⣶⣶⣿⣦⣾⣷⣾⡿⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠻⣿⠟⠉⠁⠀⣸⡟⠑⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⠃⠀⠀⠈⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀                                                    
+
+"
+
+$jamesbond ="
+     0000             0000        7777777777777777/========___________
+   00000000         00000000      7777^^^^^^^7777/ || ||   ___________
+  000    000       000    000     777       7777/=========//
+ 000      000     000      000             7777// ((     //
+0000      0000   0000      0000           7777//   \\   //
+0000      0000   0000      0000          7777//========//
+0000      0000   0000      0000         7777
+0000      0000   0000      0000        7777
+ 000      000     000      000        7777
+  000    000       000    000       77777
+   00000000         00000000       7777777
+     0000             0000        777777777
+"
+
+$choix = Read-Host -Prompt "1. Star Wars Theme
+2.Anniversaire Theme
+3.Pacman Theme
+4.Halloween Theme
+5.James Bond Theme"
+
+if ($choix -eq "1"){
+
+Write-Host "$starwars"
+
+[console]::beep(440,500)
+[console]::beep(440,500)
+[console]::beep(440,500)
+[console]::beep(349,350)
+[console]::beep(523,150)
+[console]::beep(440,500)
+[console]::beep(349,350)
+[console]::beep(523,150)
+[console]::beep(440,1000)
+[console]::beep(659,500)
+[console]::beep(659,500)
+[console]::beep(659,500)
+[console]::beep(698,350)
+[console]::beep(523,150)
+[console]::beep(415,500)
+[console]::beep(349,350)
+[console]::beep(523,150)
+[console]::beep(440,1000)
+} elseif ($choix -eq "2"){
+
+Write-Host "$anniversaire"
+
+$BeepList = @(
+@{ Pitch = 1059.274; Length = 300; };
+@{ Pitch = 1059.274; Length = 200; };
+@{ Pitch = 1188.995; Length = 500; };
+@{ Pitch = 1059.274; Length = 500; };
+@{ Pitch = 1413.961; Length = 500; };
+@{ Pitch = 1334.601; Length = 950; };
+
+
+
+@{ Pitch = 1059.274; Length = 300; };
+@{ Pitch = 1059.274; Length = 200; };
+@{ Pitch = 1188.995; Length = 500; };
+@{ Pitch = 1059.274; Length = 500; };
+@{ Pitch = 1587.117; Length = 500; };
+@{ Pitch = 1413.961; Length = 950; };
+
+
+
+@{ Pitch = 1059.274; Length = 300; };
+@{ Pitch = 1059.274; Length = 200; };
+@{ Pitch = 2118.547; Length = 500; };
+@{ Pitch = 1781.479; Length = 500; };
+@{ Pitch = 1413.961; Length = 500; };
+@{ Pitch = 1334.601; Length = 500; };
+@{ Pitch = 1188.995; Length = 500; };
+@{ Pitch = 1887.411; Length = 300; };
+@{ Pitch = 1887.411; Length = 200; };
+@{ Pitch = 1781.479; Length = 500; };
+@{ Pitch = 1413.961; Length = 500; };
+@{ Pitch = 1587.117; Length = 500; };
+@{ Pitch = 1413.961; Length = 900; };
+);
+# I Just added this For..loop in order to listen the beep tones twice (-_°)
+For ($i=1; $i -le 2; $i++) {
+foreach ($Beep in $BeepList) {
+[System.Console]::Beep($Beep['Pitch'], $Beep['Length']);
+}
+}
+
+} elseif ($choix -eq "3"){
+
+Write-Host = $pacman
+
+[Console]::Beep(658, 125);
+[Console]::Beep(1320, 500);
+[Console]::Beep(990, 250);
+[Console]::Beep(1056, 250);
+[Console]::Beep(1188, 250);
+[Console]::Beep(1320, 125);
+[Console]::Beep(1188, 125);
+[Console]::Beep(1056, 250);
+[Console]::Beep(990, 250);
+[Console]::Beep(880, 500);
+[Console]::Beep(880, 250);
+[Console]::Beep(1056, 250);
+[Console]::Beep(1320, 500);
+[Console]::Beep(1188, 250);
+[Console]::Beep(1056, 250);
+[Console]::Beep(990, 750);
+[Console]::Beep(1056, 250);
+[Console]::Beep(1188, 500);
+[Console]::Beep(1320, 500);
+[Console]::Beep(1056, 500);
+[Console]::Beep(880, 500);
+[Console]::Beep(880, 500);
+sleep -m 250;
+[Console]::Beep(1188, 500);
+[Console]::Beep(1408, 250);
+[Console]::Beep(1760, 500);
+[Console]::Beep(1584, 250);
+[Console]::Beep(1408, 250);
+[Console]::Beep(1320, 750);
+[Console]::Beep(1056, 250);
+[Console]::Beep(1320, 500);
+[Console]::Beep(1188, 250);
+[Console]::Beep(1056, 250);
+[Console]::Beep(990, 500);
+[Console]::Beep(990, 250);
+[Console]::Beep(1056, 250);
+[Console]::Beep(1188, 500);
+[Console]::Beep(1320, 500);
+[Console]::Beep(1056, 500);
+[Console]::Beep(880, 500);
+[Console]::Beep(880, 500);
+sleep -m 500;
+[Console]::Beep(1320, 500);
+[Console]::Beep(990, 250);
+[Console]::Beep(1056, 250);
+[Console]::Beep(1188, 250);
+[Console]::Beep(1320, 125);
+[Console]::Beep(1188, 125);
+[Console]::Beep(1056, 250);
+[Console]::Beep(990, 250);
+[Console]::Beep(880, 500);
+[Console]::Beep(880, 250);
+[Console]::Beep(1056, 250);
+[Console]::Beep(1320, 500);
+[Console]::Beep(1188, 250);
+[Console]::Beep(1056, 250);
+[Console]::Beep(990, 750);
+[Console]::Beep(1056, 250);
+[Console]::Beep(1188, 500);
+[Console]::Beep(1320, 500);
+[Console]::Beep(1056, 500);
+[Console]::Beep(880, 500);
+[Console]::Beep(880, 500);
+sleep -m 250;
+[Console]::Beep(1188, 500);
+[Console]::Beep(1408, 250);
+[Console]::Beep(1760, 500);
+[Console]::Beep(1584, 250);
+[Console]::Beep(1408, 250);
+[Console]::Beep(1320, 750);
+[Console]::Beep(1056, 250);
+[Console]::Beep(1320, 500);
+[Console]::Beep(1188, 250);
+[Console]::Beep(1056, 250);
+[Console]::Beep(990, 500);
+[Console]::Beep(990, 250);
+[Console]::Beep(1056, 250);
+[Console]::Beep(1188, 500);
+[Console]::Beep(1320, 500);
+[Console]::Beep(1056, 500);
+[Console]::Beep(880, 500);
+[Console]::Beep(880, 500);
+sleep -m 500;
+[Console]::Beep(660, 1000);
+[Console]::Beep(528, 1000);
+[Console]::Beep(594, 1000);
+[Console]::Beep(495, 1000);
+[Console]::Beep(528, 1000);
+[Console]::Beep(440, 1000);
+[Console]::Beep(419, 1000);
+[Console]::Beep(495, 1000);
+[Console]::Beep(660, 1000);
+[Console]::Beep(528, 1000);
+[Console]::Beep(594, 1000);
+[Console]::Beep(495, 1000);
+[Console]::Beep(528, 500);
+[Console]::Beep(660, 500);
+[Console]::Beep(880, 1000);
+[Console]::Beep(838, 2000);
+[Console]::Beep(660, 1000);
+[Console]::Beep(528, 1000);
+[Console]::Beep(594, 1000);
+[Console]::Beep(495, 1000);
+[Console]::Beep(528, 1000);
+[Console]::Beep(440, 1000);
+[Console]::Beep(419, 1000);
+[Console]::Beep(495, 1000);
+[Console]::Beep(660, 1000);
+[Console]::Beep(528, 1000);
+[Console]::Beep(594, 1000);
+[Console]::Beep(495, 1000);
+[Console]::Beep(528, 500);
+[Console]::Beep(660, 500);
+[Console]::Beep(880, 1000);
+[Console]::Beep(838, 2000);
+} elseif ($choix -eq "4"){
+
+Write-Host = $halloween
+
+[System.Console]::Beep(1109,200);
+[System.Console]::Beep(740,200);
+[System.Console]::Beep(740,200);
+[System.Console]::Beep(1109,200);
+[System.Console]::Beep(740,200);
+[System.Console]::Beep(740,200);
+[System.Console]::Beep(1109,200);
+[System.Console]::Beep(740,200);
+[System.Console]::Beep(1175,200);
+[System.Console]::Beep(740,200);
+[System.Console]::Beep(1109,200);
+[System.Console]::Beep(740,200);
+[System.Console]::Beep(740,200);
+[System.Console]::Beep(1109,200);
+[System.Console]::Beep(740,200);
+[System.Console]::Beep(740,200);
+[System.Console]::Beep(1109,200);
+[System.Console]::Beep(740,200);
+[System.Console]::Beep(1175,200);
+[System.Console]::Beep(740,200);
+[System.Console]::Beep(1109,200);
+[System.Console]::Beep(740,200);
+[System.Console]::Beep(740,200);
+[System.Console]::Beep(1109,200);
+[System.Console]::Beep(740,200);
+[System.Console]::Beep(740,200);
+[System.Console]::Beep(1109,200);
+[System.Console]::Beep(740,200);
+[System.Console]::Beep(1175,200);
+[System.Console]::Beep(740,200);
+[System.Console]::Beep(1109,200);
+[System.Console]::Beep(740,200);
+[System.Console]::Beep(740,200);
+[System.Console]::Beep(1109,200);
+[System.Console]::Beep(740,200);
+[System.Console]::Beep(740,200);
+[System.Console]::Beep(1109,200);
+[System.Console]::Beep(740,200);
+[System.Console]::Beep(1175,200);
+[System.Console]::Beep(740,200);
+[System.Console]::Beep(1109,200);
+[System.Console]::Beep(740,200);
+[System.Console]::Beep(740,200);
+[System.Console]::Beep(1109,200);
+[System.Console]::Beep(740,200);
+[System.Console]::Beep(740,200);
+[System.Console]::Beep(1109,200);
+[System.Console]::Beep(740,200);
+[System.Console]::Beep(1175,200);
+[System.Console]::Beep(740,200);
+[System.Console]::Beep(1109,200);
+[System.Console]::Beep(740,200);
+[System.Console]::Beep(740,200);
+[System.Console]::Beep(1109,200);
+[System.Console]::Beep(740,200);
+[System.Console]::Beep(740,200);
+[System.Console]::Beep(1109,200);
+[System.Console]::Beep(740,200);
+[System.Console]::Beep(1175,200);
+[System.Console]::Beep(740,200);
+[System.Console]::Beep(1109,200);
+[System.Console]::Beep(740,200);
+[System.Console]::Beep(740,200);
+[System.Console]::Beep(1109,200);
+[System.Console]::Beep(740,200);
+[System.Console]::Beep(740,200);
+[System.Console]::Beep(1109,200);
+[System.Console]::Beep(740,200);
+[System.Console]::Beep(1175,200);
+[System.Console]::Beep(740,200);p(523,150)
+}elseif ($choix -eq "5"){
+
+Write-Host = $jamesbond
+
+Start-Sleep -m 300
+[console]::beep(784,150)
+Start-Sleep -m 300
+[console]::beep(932,150)
+Start-Sleep -m 150
+[console]::beep(1047,150)
+Start-Sleep -m 150
+[console]::beep(784,150)
+Start-Sleep -m 300
+[console]::beep(784,150)
+Start-Sleep -m 300
+[console]::beep(699,150)
+Start-Sleep -m 150
+[console]::beep(740,150)
+Start-Sleep -m 150
+[console]::beep(932,150)
+[console]::beep(784,150)
+[console]::beep(587,1200)
+Start-Sleep -m 75
+[console]::beep(932,150)
+[console]::beep(784,150)
+[console]::beep(554,1200)
+Start-Sleep -m 75
+[console]::beep(932,150)
+[console]::beep(784,150)
+[console]::beep(523,1200)
+Start-Sleep -m 150
+[console]::beep(466,150)
+[console]::beep(523,150)
+
+}else{
+Write-Host "Numero tappé non-disponible!"
+}
